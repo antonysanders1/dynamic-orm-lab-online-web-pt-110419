64ed0eecf393d82635abc8cef433df3e5d784ba1
Antony Sanders <antony.devon92@gmail.com>
2020-02-06 23:44:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -77,10 +77,10 @@ class InteractiveRecord
  end 
  
  
-# def self.find_by(attribute)
-#   #binding.pry
-#   DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{attribute} = (?)")
-# end 
+ def self.find_by(attribute)
+   #binding.pry
+   DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{attribute} = (?)")
+ end 
  
  
  
